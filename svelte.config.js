27482b3a8a093c33bcee92c98c87067328a939b8
kagan <intend.on.games+git@gmail.com>
2024-03-06 23:12:22 +0900

    サブディレクトリの設定を追加

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			strict: false
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : "/html-card-game"
+		}
 	}
 };
 
