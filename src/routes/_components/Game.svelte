5636ebf65a60604de639182979528b85a2e2167e
kagan <intend.on.games+git@gmail.com>
2024-03-10 03:32:50 +0900

    oneOrMoreのケースのみ実装

--- src/routes/_components/Game.svelte
+++ src/routes/_components/Game.svelte
@@ -1,25 +1,55 @@
 <script lang="ts">
-  let current = 3;
-  let hand = [1, 2, 3, 4, 5];
-  const canPlay = (number: number) => {
-    return number > current;
+  import htmlSpec from "@markuplint/html-spec";
+  import { getContentModel } from "@markuplint/ml-spec";
+  import { createSelector } from "@markuplint/selector";
+  import { browser } from "$app/environment";
+
+  let current = "body";
+  let hand = ["div", "span", "a", "br", "p"];
+  $: contentModel = browser
+    ? getContentModel(document.createElement(current), htmlSpec.specs)
+    : null;
+  $: {
+    console.log(contentModel);
+  }
+
+  const canPlay = (tagName: string): boolean => {
+    if (browser) {
+      const element = document.createElement(tagName);
+      if (Array.isArray(contentModel)) {
+        return (
+          createSelector(contentModel[0].oneOrMore, htmlSpec).match(element) !==
+          false
+        );
+      }
+    }
+    return tagName > current;
   };
+
+  if (browser) {
+    const sample1 = document.createElement("a");
+    document.createElement("div").appendChild(sample1);
+    const sample2 = document.createElement("a");
+    document.createElement("span").appendChild(sample2);
+    // console.log(getContentModel(sample1, htmlSpec.specs));
+    // console.log(getContentModel(sample2, htmlSpec.specs));
+  }
 </script>
 
 <p>{current}</p>
 
 <ul>
   <!-- for -->
-  {#each hand as number}
+  {#each hand as el}
     <li>
       <button
         on:click={() => {
-          if (canPlay(number)) {
-            current = number;
-            hand = hand.filter((n) => n !== number);
+          if (canPlay(el)) {
+            current = el;
+            hand = hand.filter((n) => n !== el);
           }
         }}
-        disabled={!canPlay(number)}>{number}</button
+        disabled={!canPlay(el)}>{el}</button
       >
     </li>
   {/each}
