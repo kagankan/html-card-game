b48afc52f39a0940e8f6295ccc6f80dfbd8bee3c
kagan <intend.on.games+git@gmail.com>
2024-05-05 04:09:21 +0900

    記述整理
    classで書こうと思ったが、再レンダリングされないため諦め

--- src/routes/_components/Game.svelte
+++ src/routes/_components/Game.svelte
@@ -18,6 +18,26 @@
     "br",
   ];
   let match = startMatch(DEFAULT_DECK, 1);
+
+  const play = (playerIndex: number, cardIndex: number): void => {
+    const newPlayers = [...match.players];
+    newPlayers[playerIndex] = newPlayers[playerIndex].filter(
+      (_, index) => index !== cardIndex,
+    );
+    const card = match.players[playerIndex][cardIndex];
+    match = {
+      field: [...match.field, card],
+      players: newPlayers,
+    };
+  };
+
+  // 場を流す
+  const nextRound = (): void => {
+    match = {
+      ...match,
+      field: [],
+    };
+  };
 </script>
 
 <pre><code>{formatHtml(match.field, 2)}</code></pre>
@@ -34,12 +54,7 @@
 
   <button
     class="mt-8 rounded-lg bg-blue-500 px-4 py-2 text-white"
-    on:click={() => {
-      match = {
-        ...match,
-        field: [],
-      };
-    }}
+    on:click={nextRound}
   >
     場をリセット
   </button>
@@ -47,17 +62,14 @@
 
 <ul class=" mt-8 grid auto-cols-auto grid-flow-col">
   <!-- for -->
-  {#each match.players[0] as el}
+  {#each match.players[0] as el, index}
     {@const ok = browser ? checkNext(match.field, el) : false}
     <li class=" w-24 transition-transform hover:z-10 hover:-translate-y-4">
       <Card
         title={el}
         description={el === "a" ? " (hrefなし)" : ""}
         onClick={() => {
-          match = {
-            field: [...match.field, el],
-            players: [match.players[0].filter((n) => n !== el)],
-          };
+          play(0, index);
         }}
         disabled={!ok}
       />
