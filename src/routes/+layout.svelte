409ae0db468814c09e4ed7434b22bc90daa21ee2
kagan <intend.on.games+git@gmail.com>
2024-06-03 03:38:37 +0900

    GTM追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,23 @@
   import "./styles.css";
 </script>
 
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-53VW712TWK"
+  ></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-53VW712TWK");
+  </script>
+</svelte:head>
+
 <slot />
 <div class="app">
   <!-- <Header /> -->
